ff4c171e52e487c94aa1a9574814f856ebd4af7f
Lu Mike <myatkaungg31@gmail.com>
2022-11-24 06:21:37 +0000

    add vote increasement in reducer

--- redux-anecdotes/src/reducers/anecdoteReducer.js
+++ redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -22,8 +22,16 @@ const initialState = anecdotesAtStart.map(asObject)
 const reducer = (state = initialState, action) => {
   console.log('state now: ', state)
   console.log('action', action)
-
-  return state
+  switch (action.type) {
+    case 'ANECDOTES-VOTE':
+      return state.map(anecdote => action.data.id === anecdote.id
+        ? {
+          ...anecdote,
+          votes: anecdote.votes + 1
+        }
+        : anecdote)
+    default: return state
+  }
 }
 
 export default reducer
