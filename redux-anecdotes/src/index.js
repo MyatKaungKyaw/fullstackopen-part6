efe346df84468ec45d16f56428b33b3a5ebb4654
MyatKaungKyaw <myatkaungg31@gmail.com>
2022-12-03 20:02:24 +0630

    use configureStore to create store

--- redux-anecdotes/src/index.js
+++ redux-anecdotes/src/index.js
@@ -4,8 +4,12 @@ import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import App from './App'
 import reducer from './reducers/anecdoteReducer'
+import {configureStore} from '@reduxjs/toolkit'
 
-const store = createStore(reducer)
+const store = configureStore({
+  reducer,
+})
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
