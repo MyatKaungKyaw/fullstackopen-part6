6e30fa2205ff332a8b42de2a5016af89498d029d
MyatKaungKyaw <myatkaungg31@gmail.com>
2022-12-04 21:18:15 +0630

    add notification component

--- redux-anecdotes/src/App.js
+++ redux-anecdotes/src/App.js
@@ -1,9 +1,11 @@
 import AnecdoteList from './components/AnecdoteList'
 import AnecdoteForm from './components/AnecdoteForm'
+import Notification from './components/Notification'
 
 const App = () => {
   return (
     <div>
+      <Notification />
       <AnecdoteForm />
       <AnecdoteList />
     </div>
