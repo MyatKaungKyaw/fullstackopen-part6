51e21ddf880fec0db4cbd8503618727662f750c9
MyatKaungKyaw <myatkaungg31@gmail.com>
2022-12-04 21:19:07 +0630

    rm unused import and moidfy store

--- redux-anecdotes/src/index.js
+++ redux-anecdotes/src/index.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import App from './App'
-import reducer from './reducers/anecdoteReducer'
+import anecdoteSlice from './reducers/anecdoteReducer'
 import {configureStore} from '@reduxjs/toolkit'
 
 const store = configureStore({
-  reducer,
+  reducer: {
+    anecdotes : anecdoteSlice,
+  }
 })
+
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
