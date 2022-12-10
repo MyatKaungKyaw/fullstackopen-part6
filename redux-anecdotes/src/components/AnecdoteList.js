f68ea420362d28347eb3cd777862bcb6ad0e178f
MyatKaungKyaw <myatkaungg31@gmail.com>
2022-12-10 21:05:48 +0630

    implement to show notification when vote

--- redux-anecdotes/src/components/AnecdoteList.js
+++ redux-anecdotes/src/components/AnecdoteList.js
@@ -1,9 +1,22 @@
 import { useSelector, useDispatch } from 'react-redux'
 import { voteAnecdote } from '../reducers/anecdoteReducer'
+import { showNotification } from '../reducers/notificationReducer'
 
 const AnecdoteList = () => {
     const anecdotes = useSelector(reducer => [...reducer.anecdotes].sort((a, b) => b.votes - a.votes))
     const dispatch = useDispatch()
+
+    const voteClickHandler = (anecdote)=>{
+        console.log(anecdote)
+        dispatch(voteAnecdote(anecdote.id))
+        dispatch(showNotification(`voted ${anecdote.content}`))
+        setTimeout(() => {
+            dispatch(showNotification(''))
+        }, 50000);
+    }  
+
+
+
     return (
         <>
             <h2>Anecdotes</h2>
@@ -14,7 +27,7 @@ const AnecdoteList = () => {
                     </div>
                     <div>
                         has {anecdote.votes}
-                        <button onClick={() => dispatch(voteAnecdote(anecdote.id))}>vote</button>
+                        <button onClick={() => voteClickHandler(anecdote)}>vote</button>
                     </div>
                 </div>
             )}
