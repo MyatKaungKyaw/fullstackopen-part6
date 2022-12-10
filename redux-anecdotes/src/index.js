ce69f7eda0a01209bd79593abc57eff12dfc3193
MyatKaungKyaw <myatkaungg31@gmail.com>
2022-12-10 21:03:12 +0630

    add notification reducer

--- redux-anecdotes/src/index.js
+++ redux-anecdotes/src/index.js
@@ -2,17 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import App from './App'
-import anecdoteSlice from './reducers/anecdoteReducer'
+
 import {configureStore} from '@reduxjs/toolkit'
+
+import anecdoteSlice from './reducers/anecdoteReducer'
+import notificationSlice from './reducers/notificationReducer'
 
 const store = configureStore({
   reducer: {
     anecdotes : anecdoteSlice,
+    notification: notificationSlice,
   }
 })
-
-
-
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
