9b73074c5a0200ce8d34687a3c35fba79a8e34cb
MyatKaungKyaw <myatkaungg31@gmail.com>
2022-12-10 21:04:24 +0630

    implement show notifiation feature

--- redux-anecdotes/src/components/AnecdoteForm.js
+++ redux-anecdotes/src/components/AnecdoteForm.js
@@ -1,5 +1,6 @@
 import { useDispatch } from 'react-redux'
 import { createAnecdote } from '../reducers/anecdoteReducer'
+import { showNotification,resetNotification } from '../reducers/notificationReducer'
 
 const AnecdoteForm = () => {
     const dispatch = useDispatch()
@@ -9,6 +10,14 @@ const AnecdoteForm = () => {
         const anecdote = e.target.anecdote.value
         e.target.anecdote.value = ''
         dispatch(createAnecdote(anecdote))
+        showCreateNoti(anecdote)
+    }
+    
+    const showCreateNoti = (anecdote)=>{
+        dispatch(showNotification(`created ${anecdote}`))
+        setTimeout(() => {
+            dispatch(showNotification(''))
+        }, 5000);
     }
 
     return (
