fd76539e5a8a423f875844ad94b1da263b06238c
MyatKaungKyaw <myatkaungg31@gmail.com>
2022-12-12 21:38:12 +0630

    add filter component

--- redux-anecdotes/src/App.js
+++ redux-anecdotes/src/App.js
@@ -1,11 +1,13 @@
 import AnecdoteList from './components/AnecdoteList'
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
+import Filter from './components/Filter'
 
 const App = () => {
   return (
     <div>
       <Notification />
+      <Filter />
       <AnecdoteForm />
       <AnecdoteList />
     </div>
