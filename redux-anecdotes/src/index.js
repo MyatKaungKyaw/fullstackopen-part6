81352b0772fdbf58156aa4bd6cd1d689f47a778f
MyatKaungKyaw <myatkaungg31@gmail.com>
2022-12-12 21:38:26 +0630

    add filter reducer

--- redux-anecdotes/src/index.js
+++ redux-anecdotes/src/index.js
@@ -7,11 +7,13 @@ import {configureStore} from '@reduxjs/toolkit'
 
 import anecdoteSlice from './reducers/anecdoteReducer'
 import notificationSlice from './reducers/notificationReducer'
+import filterSlice from './reducers/filterReducer'
 
 const store = configureStore({
   reducer: {
     anecdotes : anecdoteSlice,
     notification: notificationSlice,
+    filter: filterSlice,
   }
 })
 
