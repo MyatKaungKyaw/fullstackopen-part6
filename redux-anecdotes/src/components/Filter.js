350fc8de4c8e4ec8471eb7cd18c094ce6cc325c2
MyatKaungKyaw <myatkaungg31@gmail.com>
2022-12-12 21:38:43 +0630

    create filter component

---
+++ redux-anecdotes/src/components/Filter.js
@@ -0,0 +1,24 @@
+import { useDispatch, useSelector } from 'react-redux'
+import { changeFilter } from '../reducers/filterReducer'
+
+const Filter = () => {
+    const dispatch = useDispatch()
+    const filter = useSelector(({ filter }) => filter)
+
+    const handleChange = (event) => {
+        const filter = event.target.value
+        dispatch(changeFilter(filter))
+        console.log(event.target.value)
+    }
+    const style = {
+        marginBottom: 10
+    }
+
+    return (
+        <div style={style}>
+            filter <input onChange={handleChange} value={filter}/>
+        </div>
+    )
+}
+
+export default Filter
