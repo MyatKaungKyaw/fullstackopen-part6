a2fce08024297747649f9e7ee0362983f238eab0
MyatKaungKyaw <myatkaungg31@gmail.com>
2023-01-04 21:18:43 +0630

    set initial value to empty array

--- redux-anecdotes/src/reducers/anecdoteReducer.js
+++ redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,14 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit'
 
 
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
+const anecdotesAtStart = []
 
 const getId = () => (100000 * Math.random()).toFixed(0)
 
@@ -36,9 +29,12 @@ const anecdoteSlice = createSlice({
     },
     createAnecdote(state, action) {
       return state.concat(asObject(action.payload))
+    },
+    setAnecdote(state,action){
+      return action.payload
     }
   }
 })
 
-export const { voteAnecdote, createAnecdote } = anecdoteSlice.actions
+export const { voteAnecdote, createAnecdote, setAnecdote } = anecdoteSlice.actions
 export default anecdoteSlice.reducer
