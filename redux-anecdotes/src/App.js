5b120e5ff13200723507afb11f5a1883bccd48a0
MyatKaungKyaw <myatkaungg31@gmail.com>
2023-01-04 21:19:07 +0630

    get all initial value from db.json

--- redux-anecdotes/src/App.js
+++ redux-anecdotes/src/App.js
@@ -2,8 +2,17 @@ import AnecdoteList from './components/AnecdoteList'
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
 import Filter from './components/Filter'
+import anecdotes from './services/anecdotes'
+import {useDispatch} from 'react-redux'
+import { useEffect } from 'react'
+import { setAnecdote } from './reducers/anecdoteReducer'
 
 const App = () => {
+  const dispatch = useDispatch()
+  useEffect(()=>{
+    anecdotes.getAll().then(anecdotes => dispatch(setAnecdote(anecdotes)))
+  },[]) // eslint-disable-line react-hooks/exhaustive-deps  
+
   return (
     <div>
       <Notification />
